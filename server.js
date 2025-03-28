ccfa58516505b79fd9bc17c65fab0f9d14889b40
Kanukolanu Manjith <manjithkanukolanu87@gmail.com>
2025-03-28 17:39:26 +0530

    frontend setup

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 const app = express()
 require('dotenv').config()
 const MongodbUrl = process.env.Url
@@ -7,6 +8,10 @@ mongoose.connect(MongodbUrl)
         .then(()=>{console.log('Database Connected Successfully')})
         .catch(()=>{console.log('Database DisConnected')})
 const Port = process.env.port
+app.set('view engine','ejs')
+app.set('views', path.join(__dirname, 'views'));
+app.use('/css', express.static(path.join(__dirname, 'assests/css')));
+app.use('/img', express.static(path.join(__dirname, 'assests/img')));
 app.listen((Port)=>{
     console.log(`listening on port ${Port}`)
 })
